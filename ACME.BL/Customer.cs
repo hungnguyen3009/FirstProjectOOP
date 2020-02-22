2b1f5e103feabd6a5ea6972f312f15fb8474b105
hung.nguyen <hung.nguyen@saleshub.vn>
2020-02-22 11:51:15 +0700

    Building entity classes

--- ACME.BL/Customer.cs
+++ ACME.BL/Customer.cs
@@ -6,7 +6,33 @@ namespace ACME.BL
 {
     public class Customer
     {
-        private string _lastName;
+        public int Id { get; private set; }
+        public string FirstName { get; set; }
+        public string Email { get; set; }
+        public string FullName
+        {
+            get
+            {
+                return LastName + ", " + FirstName;
+            }
+        }
 
+        private string _lastName;
+        public string LastName
+        {
+            get
+            {
+                return _lastName;
+            }
+            set
+            {
+                _lastName = value;
+            }
+        }
+
+        // Share this InstanceCount property to all Customer objects
+        // Using static modifier for property means that this property
+        // belongs to the class itself, not objects.
+        public static int InstanceCount { get; set; }
     }
 }
---
+++ ACME.BLTest/CustomerTest.cs
@@ -0,0 +1,89 @@
+﻿using ACME.BL;
+using System;
+using System.Collections.Generic;
+using System.Text;
+using Xunit;
+
+namespace ACME.BLTest
+{
+    public class CustomerTest
+    {
+        [Fact]
+        public void FullNameTestMethod()
+        {
+            // Arrange
+            var customer = new Customer
+            {
+                FirstName = "Bilbo",
+                LastName = "Baggins"
+            };
+
+            string expected = "Baggins, Bilbo";
+
+            // Act
+            string actual = customer.FullName;
+
+            // Assert
+            Assert.Equal(expected, actual);
+        }
+
+        [Fact]
+        public void FullNameFirstNameEmpty()
+        {
+            // Arrange
+            var customer = new Customer
+            {
+                LastName = "Baggins"
+            };
+
+            string expected = "Baggins, ";
+
+            // Act
+            string actual = customer.FullName;
+
+            // Assert
+            Assert.Equal(expected, actual);
+        }
+
+        [Fact]
+        public void FullNameLastNameEmpty()
+        {
+            // Arrange
+            var customer = new Customer
+            {
+                FirstName = "Bilbo"
+            };
+
+            string expected = ", Bilbo";
+
+            // Act
+            string actual = customer.FullName;
+
+            // Assert
+            Assert.Equal(expected, actual);
+        }
+
+        [Fact]
+        public void StaticTest()
+        {
+            // Arrange
+            var c1 = new Customer();
+            c1.FirstName = "Bilbo";
+            Customer.InstanceCount += 1;
+
+            var c2 = new Customer();
+            c2.FirstName = "Frodo";
+            Customer.InstanceCount += 1;
+
+            var c3 = new Customer();
+            c3.FirstName = "Rosie";
+            Customer.InstanceCount += 1;
+
+            // Act
+
+
+            // Assert
+            Assert.Equal(3, Customer.InstanceCount);
+        }
+    }
+}
